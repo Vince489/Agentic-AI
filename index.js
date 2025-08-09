c9a376eb29bb50d692de80e8fcf25bd7f4424f25
Vince489 <vincentcornelius489@gmail.com>
2025-08-09 10:21:44 -0400

    index added

---
+++ index.js
@@ -0,0 +1,7 @@
+export { Agent } from './Agent.js';
+export { AgentFactory } from './AgentFactory.js';
+export { Agency } from './Agency.js';
+export { AgencyFactory } from './AgencyFactory.js';
+export { Team } from './Team.js';
+export { TeamFactory } from './TeamFactory.js';
+export { GeminiProvider } from './GeminiProvider.js';
